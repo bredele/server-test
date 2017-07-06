a66ea0fa729d797e5d83025f492f52b0c73e49db
bredele <olivier.wietrich@gmail.com>
2017-07-05 22:28:51 -0600

    Add basic HTTP server with request manager

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const net = require('net')
 const request = require('request')
 
 
-module.exports = function (cb) {
 
+/**
+ * Create HTTP server and send request using the npm module
+ * request/request
+ *
+ * @param {Function} cb
+ * @param {Object} data
+ * @api public
+ */
+
+module.exports = function (cb, data) {
+  const server = http.createServer((req, res) => {
+    cb(req, res)
+    res.end()
+  }).listen(() => {
+    const port = server.address().port
+    const sock = net.connect(port)
+    request(data, () => {
+      sock.end()
+      server.close()
+    })
+  })
 }
